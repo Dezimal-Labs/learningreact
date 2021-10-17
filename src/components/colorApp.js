737d5d2b49535abf7cd1c4f2a29eeb5edc753d80
jabeermk <jabeermk@gmail.com>
2021-10-17 13:04:28 +0530

    add color

--- src/components/colorApp.js
+++ src/components/colorApp.js
@@ -2,14 +2,35 @@
 import React, { useState } from 'react'
 import ColorList from './colorList.js'
 import colorData from '../data/color-data.json'
+import AddColorForm from './colorForm.js';
+import { v4 } from "uuid";
 
 export default function ColorApp() {
 
     const [colors, setColors] = useState(colorData);
 
-    return <ColorList colors={colors}
-    onRemoveColor={(id) => {
-       const newColors = colors.filter((c) => c.id !== id)
-       setColors(newColors);
-   }}/>;
+    return (
+        <>
+            <AddColorForm
+                onNewColor={(title, color) => {
+                    const newColors = [
+                        ...colors,
+                        {
+                            id: v4(),
+                            rating: 0,
+                            title,
+                            color
+                        }
+                    ];
+                    setColors(newColors);
+                }}
+            />
+
+            <ColorList colors={colors}
+                onRemoveColor={(id) => {
+                    const newColors = colors.filter((c) => c.id !== id)
+                    setColors(newColors);
+                }} />
+        </>
+    )
 }
