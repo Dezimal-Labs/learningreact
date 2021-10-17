c6aefa3db86a9b4403f9427ac4dc8ec73056269f
jabeermk <jabeermk@gmail.com>
2021-10-17 13:35:19 +0530

    colorrating

--- src/components/colorList.js
+++ src/components/colorList.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Color from './color.js'
 
-export default function colorList({ colors = [], onRemoveColor = (f) => f }) {
+export default function colorList({ colors = [], onRemoveColor = (f) => f,onRateColor = f => f }) {
     if (!colors.length) return <div>No color</div>
 
     return (
         <div>
-            {colors.map(c => <Color key={c.id} {...c} onRemove={onRemoveColor} />)}
+            {colors.map(c => <Color key={c.id} {...c} onRemove={onRemoveColor} onRate={onRateColor} />)}
         </div>
 
     )
--- src/components/starRating.js
+++ src/components/starRating.js
@@ -82,13 +82,13 @@ const createArray = (length) => [...Array(length)];
 // }
 
 
-export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
+export default function StarRating({ totalStars = 5, selectedStars = 0 ,onRate = f => f}) {
     
 
     return (<>
 
         {
-            createArray(totalStars).map((n, i) => <Star key={i} selected={selectedStars > i} />)
+            createArray(totalStars).map((n, i) => <Star key={i} selected={selectedStars > i} onSelect={() => onRate(i + 1)} />)
         }
         <p>
             {selectedStars} of {totalStars} stars
