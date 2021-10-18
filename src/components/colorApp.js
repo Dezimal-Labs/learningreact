c33ecbed35e557f5155e3436fb3ba4f7308e27b9
jabeermk <jabeermk@gmail.com>
2021-10-18 17:13:07 +0530

    colorAppUsingHook

--- src/components/colorApp.js
+++ src/components/colorApp.js
@@ -1,12 +1,8 @@
 
-import React, { useState } from 'react'
+import React from 'react'
 import ColorList from './colorList.js'
-import colorData from '../data/color-data.json'
 import AddColorForm from './colorForm.js';
-import { v4 } from "uuid";
-import UseColorHook from '../hooks/useColorHook.js';
 import useColorHook from '../hooks/useColorHook.js';
-
 export default function ColorApp() {
 
     const [colors, onAddColor, onRateColor, onRemoveColor] =useColorHook();
