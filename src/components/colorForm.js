9a8afa55fb1bfefb3592c3f0f7f3f1c0dfbac809
jabeermk <jabeermk@gmail.com>
2021-10-18 17:14:14 +0530

    colorAppUsingHook

--- src/components/colorForm.js
+++ src/components/colorForm.js
@@ -4,31 +4,31 @@ import { useInput } from "../hooks/useInputHook";
 export default function AddColorForm({ onNewColor = f => f }) {
     const [titleProps, resetTitle] = useInput("");
     const [colorProps, resetColor] = useInput("#000000");
-    
+
 
     const submit = event => {
         event.preventDefault();
         onNewColor(titleProps.value, colorProps.value);
         resetTitle();
         resetColor();
-        };
-        
+    };
+
 
 
     return (
         <form onSubmit={submit}>
-        <input
-        {...titleProps}
-        type="text"
-        placeholder="color title..."
-        required
-        />
-        <input {...colorProps} type="color" required />
-        
-        <button>ADD</button>
+            <input
+                {...titleProps}
+                type="text"
+                placeholder="color title..."
+                required
+            />
+            <input {...colorProps} type="color" required />
+
+            <button>ADD</button>
         </form>
-        );
-        
+    );
+
 }
 
 
